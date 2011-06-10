cddeae2e06e72c061c47bfdcbd7cc5cb0d8586ca
fleitner <fleitner.cnio@gmail.com>
2011-06-10 21:09:51 +0200

    autodoc now shows both class and __init__ docstrings

--- doc/conf.py
+++ doc/conf.py
@@ -247,3 +247,7 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}
+
+# -- Options for autodoc ------
+
+autoclass_content = "both"
