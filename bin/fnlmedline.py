eebaa94c090dac1fecfe627204ab420d2a8e0e6d
fleitner <fleitner.cnio@gmail.com>
2011-06-30 17:05:05 +0200

    actions now globals, better digits check for PMIDs

--- bin/fnlmedline.py
+++ bin/fnlmedline.py
@@ -12,37 +12,52 @@ __author__ = "Florian Leitner"
 __version__ = "0.1"
 
 CREATE = 1
-READ = 2
+EXTRACT = 2
 UPDATE = 3
 REPLACE = 4
 DELETE = 5
+
+ACTIONS = {
+    1: 'creat',
+    2: 'extract',
+    3: 'updat',
+    4: 'replac',
+    5: 'delet',
+}
 
 def main(pmids:list, action:int=CREATE, couch_db:str=COUCHDB_URL,
          database:str='medline'):
     db = Server(couch_db)[database]
     checked = False
 
-    if not pmids and action in (UPDATE, READ):
+    if not pmids and action in (UPDATE, EXTRACT):
         pmids = list(db) # read/update all records...
         checked = True
 
-    logging.info("processing %i PMIDs", len(pmids))
+    logging.info("%sing %i PMIDs", ACTIONS[action], len(pmids))
+    processed_docs = 0
 
     if action is DELETE:
         for id in pmids:
-            if checked or id in db: del db[id]
-            else: logging.warn("PMID {} not in DB".format(id))
-    elif action is READ:
+            if checked or id in db:
+                del db[id]
+                processed_docs += 1
+            else:
+                logging.warn("PMID {} not in DB".format(id))
+    elif action is EXTRACT:
         for id in pmids:
             if checked or id in db:
                 att = db.getAttachment(id, ABSTRACT_FILE)
                 file = open("{}.txt".format(id), mode='w', encoding='utf-8')
                 file.write(att.data)
                 file.close()
+                processed_docs += 1
             else:
                 logging.warn("PMID {} not in DB".format(id))
     else:
-        Dump(pmids, db, action is UPDATE, action is REPLACE)
+        processed_docs = Dump(pmids, db, action is UPDATE, action is REPLACE)
+
+    logging.info("%sed %i PMIDs", ACTIONS[action], processed_docs)
 
     return 0
 
@@ -61,7 +76,7 @@ if __name__ == '__main__':
     parser.set_defaults(action=CREATE)
 
     parser.add_option(
-        "-x", "--read", action="store_const", const=READ, dest="action",
+        "-x", "--extract", action="store_const", const=EXTRACT, dest="action",
         help="save the abstracts (incl. titles) of existing records to files "\
              "named <PMID>.txt in the current directory; "\
              "extracts all records if no arguments"
@@ -72,7 +87,7 @@ if __name__ == '__main__':
              "completed; updates all records if no arguments"
     )
     parser.add_option(
-        "-f", "--replace", action="store_const", const=REPLACE, dest="action",
+        "-r", "--replace", action="store_const", const=REPLACE, dest="action",
         help="download all records, even if they are stored already"
     )
     parser.add_option(
@@ -80,10 +95,12 @@ if __name__ == '__main__':
         help="delete records from the database"
     )
     parser.add_option(
-        "--couch-db", help="URL of the Couch Server [{}]".format(COUCHDB_URL)
+        "--couch-db", default=COUCHDB_URL,
+        help="URL of the Couch Server [%default]"
     )
     parser.add_option(
-        "--database", help="name of the Couch DB to use [medline]"
+        "--database", default='medline',
+        help="name of the Couch DB to use [%default]"
     )
     parser.add_option(
         "--error", action="store_const", const=logging.ERROR,
@@ -100,7 +117,7 @@ if __name__ == '__main__':
     parser.add_option("--logfile", help="log to file, not STDERR")
 
     opts, args = parser.parse_args()
-    if len(args) < 1 and opts.action not in (UPDATE, READ):
+    if len(args) < 1 and opts.action not in (UPDATE, EXTRACT):
         parser.error("no input files or PMIDs")
 
     pmids = []
@@ -109,11 +126,11 @@ if __name__ == '__main__':
         try:
             if os.path.isfile(item):
                 with open(item) as infile:
-                    pmids.extend(str(int(line)) for line in infile)
+                    pmids.extend(line for line in infile if line.isdigit())
+            elif item.isdigit():
+                pmids.append(item)
             else:
-                pmids.append(str(int(item)))
-        except ValueError:
-            parser.error("{} not PMID or file".format(item))
+                parser.error("{} not PMID or file".format(item))
         except IOError:
             parser.error("could not read {}".format(item))
 
