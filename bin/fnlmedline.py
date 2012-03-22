a09928d9cf0c29dbc79fc324436be67715f4277f
fnl <florian.leitner@gmail.com>
2012-03-22 12:39:29 +0100

    improved TSV output: do not quote integers and floats, and JSON-encode values that are not strings (i.e. now it is possible to extract fields with their nested structure)

--- bin/fnlmedline.py
+++ bin/fnlmedline.py
@@ -5,9 +5,11 @@ from _socket import error
 
 import logging
 import os
+import re
 import sys
 from libfnl.couch import COUCHDB_URL, Server
 from libfnl.couch.network import ResourceNotFound
+from libfnl.couch.serializer import Encode
 from libfnl.nlp.medline import Attach, Dump, ABSTRACT_ELEMENTS
 
 __author__ = "Florian Leitner"
@@ -29,6 +31,8 @@ ACTIONS = {
     READ_ATT: 'extract',
 }
 
+NULL_VALUE = '\\N'
+
 def main(pmids:list, action:int=CREATE, couchdb_url:str=COUCHDB_URL,
          database:str='medline', encoding:str='utf-8', extract_fields=None,
          force:bool=False) -> int:
@@ -43,20 +47,24 @@ def main(pmids:list, action:int=CREATE, couchdb_url:str=COUCHDB_URL,
 
     done = 0
 
-    if extract_fields:
+    if action is READ and extract_fields:
+        # Print the TSV table header as the field names
         print('PMID\t{}'.format('\t'.join(extract_fields)))
 
         for idx in range(len(extract_fields)):
-            extract_fields[idx] = extract_fields[idx].split('/')
+            extract_fields[idx] = [int(f) if f.isnumeric() else f
+                                   for f in extract_fields[idx].split('/')]
 
     if action == ATTACH:
+        # "attach" (fulltext) files to PubMed records in the DB
         done = sum(len(i) for i in Attach(pmids, db, encoding, force).values())
     else:
         if not pmids and action in (UPDATE, READ, READ_ATT):
-            # read/update all records...
+            # command to read/update *all* records: collect the known PMIDs
             pmids = [id for id in db if len(id) <= 10 and id.isdigit()]
 
         if action is DELETE:
+            # delete PubMed records from the DB
             for id in pmids:
                 try:
                     del db[id]
@@ -65,61 +73,29 @@ def main(pmids:list, action:int=CREATE, couchdb_url:str=COUCHDB_URL,
                     logging.warn("PMID {} not in DB".format(id))
                     print(id, file=sys.stderr)
         elif action is READ:
+            # read PubMed records from the DB (and write to file system)
             for id in pmids:
-                if extract_fields:
-                    try:
-                        record = db[id]
-                    except ResourceNotFound:
-                        logging.warn("PMID %s not in DB", id)
-                        print(id, file=sys.stderr)
-                        continue
-
-                    data = [id]
-
-                    for f_path in extract_fields:
-                        val = record
-
-                        for f in f_path:
-                            try:
-                                val = val[f]
-                            except KeyError:
-                                logging.warn("PMID %s has no %s in %s",
-                                             id, f, f_path)
-                                print(id, file=sys.stderr)
-                                val = None
-                                break
-
-                        if val is None:
-                            data = None
-                            break
-
-                        if f_path == ['Article', 'Abstract']:
-                            tmp = []
-
-                            for key in ABSTRACT_ELEMENTS:
-                                if key in val:
-                                    tmp.append(val[key])
-                            val = '\n\n'.join(tmp)
-
-                        val = val.replace('\n', '\\n')
-                        data.append(str(val.replace('"', '\\"')))
-
-                    if data is None: continue
-                    print('"{}"'.format('"\t"'.join(data)))
-                    done += 1
+                try:
+                    record = db[id]
+                except ResourceNotFound:
+                    logging.warn("PMID %s not in DB", id)
+                    print(id, file=sys.stderr)
                 else:
-                    try:
-                        text = db[id]['text']
-                    except ResourceNotFound:
-                        logging.warn("PMID %s not in DB", id)
-                        print(id, file=sys.stderr)
-                    except KeyError:
-                        logging.warn("PMID %s has no text", id)
-                        print(id, file=sys.stderr)
+                    if extract_fields:
+                        values = Extract(record, extract_fields)
+                        print('{}'.format('\t'.join(values)))
+                        done += 1
                     else:
-                        done += WriteFile('{}.txt'.format(id),
-                                          text, encoding, id)
+                        try:
+                            text = record['text']
+                        except KeyError:
+                            logging.warn("PMID %s has no text", id)
+                            print(id, file=sys.stderr)
+                        else:
+                            done += WriteFile('{}.txt'.format(id),
+                                              text, encoding, id)
         elif action is READ_ATT:
+            # read attachment plain-text from the DB (and write to file system)
             for id in pmids:
                 try:
                     rows = list(db.view('attachment/xrefs', key=id))
@@ -138,6 +114,7 @@ def main(pmids:list, action:int=CREATE, couchdb_url:str=COUCHDB_URL,
                                           text, encoding,
                                           '{}@{}'.format(id, r.id))
         else:
+            # create/update PubMed records in the DB
             done, failed_ids = Dump(pmids, db, action is UPDATE, force)
 
             for id in failed_ids:
@@ -147,6 +124,57 @@ def main(pmids:list, action:int=CREATE, couchdb_url:str=COUCHDB_URL,
                  'PMIDs' if action else 'files')
 
     return 0
+
+def Extract(doc, fields):
+    """
+    Extract the given *fields* from the *doc* and return these fields'
+    values.
+
+    :param doc: a PubMed/MEDLINE :class:`libfnl.couch.broker.Document` from the
+                DB
+    :param fields: a list of lists with the paths to each field to extract
+    :return: a list of strings; if the value is an integer or float, it is
+             given unquoted; if the field value was not a string, it is
+             JSON-encoded and quoted; otherwise, it is the quoted string value
+    """
+    data = [doc.id]
+
+    for f_path in fields:
+        val = doc
+
+        for f in f_path:
+            try:
+                val = val[f]
+            except KeyError:
+                logging.warn("PMID %s has no field '%s' in %s",
+                             doc.id, f, f_path)
+                val = None
+
+        if val is None:
+            data.append(NULL_VALUE)
+            continue
+
+        if f_path == ['Article', 'Abstract']:
+            val = '\n\n'.join([val[key] for key in ABSTRACT_ELEMENTS
+                               if key in val])
+
+        if not isinstance(val, str): val = Encode(val)
+
+        if val.isnumeric():
+            pass # an integer value - nothing to do
+        elif re.match('\d*\.\d+', val, re.A):
+            pass # a float value - nothing to do, either
+        else:
+            # a string value - replace newlines, tabs, quotes
+            val = val.replace('\n', '\\n')
+            val = val.replace('\t', '\\t')
+            val = val.replace('"', '\\"')
+            # surround the string with quotes
+            val = '"{}"'.format(val)
+
+        data.append(val)
+
+    return data
 
 def WriteFile(name, text, encoding, id):
     try:
