f0fa5f260f4c2b48e48c7347fe67aefb4954e569
fnl <florian.leitner@gmail.com>
2013-08-28 18:13:41 +0200

    tsv output, more CLI cleaning

--- bin/fnlmedline.py
+++ bin/fnlmedline.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
-"""Maintain a MEDLINE/PubMed repository:
-
-"parse"   PubMed XML files into raw table files for DB dumping -
-"insert"  PubMed XML files or a list of PMIDs (contacting EUtils) into the DB
-          (but slower than using "parse" and a DB dump) -
-"update"  existing or add new records from PubMed XML files or a list of PMIDs (slow!) -
-"write"   DB records to a directory starting from a given list of PMIDs -
-"delete"  records from the DB for a given list of PMIDs"""
+#
+# Maintain a MEDLINE/PubMed repository
+#
+"""
+parse:   PubMed XML files into raw table files for DB dumping; ==
+insert:  PubMed XML files or a list of PMIDs (contacting EUtils) into the DB
+         (slower than using "parse" and a DB dump); ==
+update:  existing records or add new records from PubMed XML files or a list of PMIDs
+         (slow!); ==
+write:   DB records to a directory starting from a given list of PMIDs; ==
+delete:  records from the DB for a given list of PMIDs
+"""
 import logging
 import os
 import sys
@@ -37,13 +41,27 @@ def Main(command, files_or_pmids, session, uniq=False):
         return delete(session, [int(i) for i in files_or_pmids])
 
 
-def WriteRecords(query, tiab:bool, output_dir:str):
+def WriteRecords(query, format:str, output_dir:str):
+    if format == 'tab':
+        file = open(output_dir, 'wt') if output_dir != '.' else sys.stdout
+
     for rec in query:
-        logging.debug("writing%s PMID %i", " TIAB from" if tiab else "", rec.pmid)
+        logging.debug("writing PMID %i as %s", rec.pmid, format)
+
+        if format == 'tab':
+            title = ' '.join(
+                [s.content for s in rec.sections if s.name == 'Title']
+            ).replace('\n', ' ').replace('\t', ' ')
+            abstract = ' '.join([s.content for s in rec.sections if s.name not in (
+                'Title', 'Vernacular', 'Copyright'
+            )]).replace('\n', ' ').replace('\t', ' ')
+            print(rec.pmid, title, abstract, sep='\t', file=file)
+            continue
+
         file = open(os.path.join(output_dir, "{}.txt".format(rec.pmid)), 'wt')
         WriteSection(file, rec, 'Title')
 
-        if not tiab:
+        if format == 'full':
             WriteSection(file, rec, 'Vernacular')
 
             for author in rec.authors:
@@ -55,7 +73,7 @@ def WriteRecords(query, tiab:bool, output_dir:str):
                     'Conclusions', 'Unlabelled'):
             WriteSection(file, rec, sec)
 
-        if not tiab:
+        if format == 'full':
             WriteSection(file, rec, 'Copyright')
             for desc in rec.descriptors:
                 print('+' if desc.major else '-', desc.name, file=file)
@@ -64,6 +82,11 @@ def WriteRecords(query, tiab:bool, output_dir:str):
                 print('', file=file)
             for ns, i in rec.identifiers.items():
                 print('{}:{}'.format(ns, i.value), file=file)
+
+        file.close()
+
+    if format == 'tab' and output_dir != '.':
+        file.close()
 
 
 def WriteSection(file, rec, sec):
@@ -89,19 +112,25 @@ if __name__ == '__main__':
     parser.set_defaults(loglevel=logging.WARNING)
 
     parser.add_argument(
-        'url', metavar='URL', help='a database URL string'
+        'url', metavar='URL', help='a database URL string, e.g.: postgresql://host/db'
     )
     parser.add_argument(
-        'command', metavar='CMD', help='command: [parse|insert|write|update|delete]'
+        'command', metavar='CMD', choices=['parse', 'insert', 'write', 'update', 'delete'],
+        help='one of {parse,insert,write,update,delete}; see above'
     )
     parser.add_argument(
-        'files', metavar='FILE/PMID', nargs='+', help='MEDLINE XML files or PMIDs'
+        'files', metavar='FILE/PMID', nargs='+', help='MEDLINE XML files or PMIDs [lists]'
     )
     parser.add_argument('--version', action='version', version=__version__)
-    parser.add_argument('--tiab', action='store_true', help='write only title/abstract')
+    parser.add_argument(
+        '--format', choices=['full', 'tiab', 'tab'], default='full',
+        help='full: [default] write all content to one file per PMID; ' +
+        'tiab: write only TIAB to files; tab: only write PMID-TI-AB to one single TSV list/file'
+        'tab: only write PMID-TI-AB to one single .tsv list/file'
+    )
     parser.add_argument(
         '--uniq', action='store_true',
-        help='do not insert/dump duplicate records'
+        help='do not parse/insert duplicate records/PMIDs (slow!)'
     )
     parser.add_argument(
         '--pmid-lists', action='store_true',
@@ -109,7 +138,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--output', metavar='DIR', default=os.path.curdir,
-        help='dump/write to a specific directory'
+        help='dump/write to a specific directory (or file for format "tab")'
     )
     parser.add_argument(
         '--error', action='store_const', const=logging.ERROR,
@@ -149,7 +178,7 @@ if __name__ == '__main__':
         result = Main(args.command, args.files, Session(), args.uniq)
 
         if args.command == 'write':
-            WriteRecords(result, args.tiab, args.output)
+            WriteRecords(result, args.format, args.output)
             result = True
 
     sys.exit(0 if result else 1)
