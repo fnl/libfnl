1f8f1a98e63e4e57735c9818423b7fedeed7e59e
Florian Leitner <florian.leitner@gmail.com>
2014-03-21 14:22:36 +0100

    updated the headers (adding GPL license statements and unified one-line descriptors of each tool)

--- scripts/fnlclassi.py
+++ scripts/fnlclassi.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+"""classi is a tool for text classification"""
+
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
