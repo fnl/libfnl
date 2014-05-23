ed2967b2b72316c6c854a1925b5339c88a343696
Florian Leitner <florian.leitner@gmail.com>
2014-05-23 15:06:53 +0200

    a better short description of the tool

--- scripts/fnldictag.py
+++ scripts/fnldictag.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-"""dictag tags tokens with keys mapped to strings in a dictionary"""
+"""dictag linguistically analyzes text and tags entities [and nouns] with dictionary keys"""
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
